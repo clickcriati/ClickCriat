ff6d11eaf6b1e90998baa45c994ba81f6d999cb3
clickcriati <francisca.gomes246@aluno.ce.gov.br>
2024-12-02 09:11:46 -0300

    Delete canva.js

--- canva.js
+++
@@ -1,8 +0,0 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.ul3');
-     if ( menuMobile.style.display == "flex") {
-        menuMobile.style.display = "none";
-    } else {
-        menuMobile.style.display = "flex";
-    }
-}
